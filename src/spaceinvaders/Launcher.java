130b62f10b47100e03aca2e5ed0540f8cb559a41
douglas <douglas@douglas-Inspiron-3442>
2016-09-25 20:01:59 -0300

    Projeto Criado

---
+++ src/spaceinvaders/Launcher.java
@@ -0,0 +1,18 @@
+package spaceinvaders;
+
+/**
+ *
+ * @author douglas
+ * @version 1- 25/09/2016, 19:36:41
+ * @languageOfComments: Portuguese
+ */
+public class Launcher {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
