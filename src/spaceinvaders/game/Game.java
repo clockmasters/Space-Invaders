6375be9ba92cef31f9f5db713471a2c085cdb8ff
douglas <douglas@douglas-Inspiron-3442>
2016-09-26 22:47:36 -0300

    Getters e Setters de atributos do tipo String, ArrayList, boolean, int, long e double criados. E Alteração de nomes de atributos, boolean.

--- src/spaceinvaders/game/Game.java
+++ src/spaceinvaders/game/Game.java
@@ -3,6 +3,8 @@ package spaceinvaders.game;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferStrategy;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import java.util.ArrayList;
 
@@ -17,41 +19,41 @@ import spaceinvaders.game.teclado.TratadorDeEventos;
  */
 public class Game {
 
-    private BufferStrategy estrategia;
+    private BufferStrategy estrategia;//
     private boolean jogoRodando;
     private ArrayList entidades;
     private ArrayList removeLista;
-    private Entidade nave;
-    private double velocidadeMovimento;
+    private Entidade nave;//
+    private double velocidadeDeMovimento;
     private long ultimoTiro;
     private long intervaloDeTiro;
     private int allienCont;
 
     private String mensagem;
-    private boolean esperarPorTeclaPress;
-    private boolean teclaEsquerda;
-    private boolean teclaDireita;
-    private boolean teclaTiro;
+    private boolean esperarPorPressionarTecla;
+    private boolean esquerdaPressionada;
+    private boolean direitaPressionada;
+    private boolean tiroPressionado;
     private boolean logicaNecessariaNesteLoop;
 
     public Game() {
         jogoRodando = true;
         entidades = new ArrayList();
         removeLista = new ArrayList();
-        velocidadeMovimento = 300;
+        velocidadeDeMovimento = 300;
         ultimoTiro = 0;
         intervaloDeTiro = 500;
 
         mensagem = "";
-        esperarPorTeclaPress = true;
-        teclaEsquerda = false;
-        teclaDireita = false;
-        teclaTiro = false;
+        esperarPorPressionarTecla = true;
+        esquerdaPressionada = false;
+        direitaPressionada = false;
+        tiroPressionado = false;
         logicaNecessariaNesteLoop = false;
 
         Display tela = new Display("Space Invaders", 600, 800);
 
-        tela.addKeyListener(new TratadorDeEventos());//Substituir o nome KeyInputHandler por PT-BR
+        tela.addKeyListener(new TratadorDeEventos(this));//Substituir o nome KeyInputHandler por PT-BR
         tela.requestFocus();
         tela.setIgnoreRepaint(true);
         tela.createBufferStrategy(2);
@@ -60,13 +62,13 @@ public class Game {
         initEntidades();
     }
 
-    private void startGame() {
+    public void startGame() {
         entidades.clear();
         initEntidades();
 
-        teclaEsquerda = false;
-        teclaDireita = false;
-        teclaTiro = false;
+        esquerdaPressionada = false;
+        direitaPressionada = false;
+        tiroPressionado = false;
     }
 
     private void initEntidades() {
@@ -94,7 +96,7 @@ public class Game {
 
     public void notificaMorte() {
         mensagem = "Ahh não! Eles te pegaram, quer tentar de novo?";
-        esperarPorTeclaPress = true;
+        esperarPorPressionarTecla = true;
     }
 
     public void notificaVitoria() {
@@ -141,7 +143,7 @@ public class Game {
             g.setColor(Color.black);
             g.fillRect(0, 0, 800, 600);
 
-            if (!esperarPorTeclaPress) {
+            if (!esperarPorPressionarTecla) {
                 for (i = 0; i < entidades.size(); i++) {
                     Entidade entidade = (Entidade) entidades.get(i);
                     entidade.move(delta);
@@ -175,7 +177,7 @@ public class Game {
                 logicaNecessariaNesteLoop = false;
             }
 
-            if (esperarPorTeclaPress) {
+            if (esperarPorPressionarTecla) {
                 g.setColor(Color.white);
                 g.drawString(mensagem, (800 - g.getFontMetrics().stringWidth(mensagem)) / 2, 250);
                 g.drawString("Press any key", (800 - g.getFontMetrics().stringWidth("Press any key")) / 2, 300);
@@ -185,20 +187,124 @@ public class Game {
             estrategia.show();
 
             nave.setMovimentoHorizontal(0);
-            if ((teclaEsquerda) && (!teclaDireita)) {
-                nave.setMovimentoHorizontal(-velocidadeMovimento);
-            } else if ((!teclaEsquerda) && (teclaDireita)) {
-                nave.setMovimentoHorizontal(velocidadeMovimento);
+            if ((esquerdaPressionada) && (!direitaPressionada)) {
+                nave.setMovimentoHorizontal(-velocidadeDeMovimento);
+            } else if ((!esquerdaPressionada) && (direitaPressionada)) {
+                nave.setMovimentoHorizontal(velocidadeDeMovimento);
             }
 
-            if (teclaTiro) {
+            if (tiroPressionado) {
                 tentaAtirar();
             }
-            
+
             try {
                 Thread.sleep(10);
             } catch (Exception e) {
             }
         }
+    }
+
+    public void setJogoRodando(boolean jogoRodando) {
+        this.jogoRodando = jogoRodando;
+    }
+
+    public boolean getJogoRodando() {
+        return jogoRodando;
+    }
+
+    public void setEsperarPorPressionarTecla(boolean esperarPorPressionarTecla) {
+        this.esperarPorPressionarTecla = esperarPorPressionarTecla;
+    }
+
+    public boolean getEsperarPorPressionarTecla() {
+        return esperarPorPressionarTecla;
+    }
+
+    public void setEsquerdaPressionada(boolean esquerdaPressionada) {
+        this.esquerdaPressionada = esquerdaPressionada;
+    }
+
+    public boolean getEsquerdaPressionada() {
+        return esquerdaPressionada;
+    }
+
+    public void setDireitaPressionada(boolean direitaPressionada) {
+        this.direitaPressionada = direitaPressionada;
+    }
+
+    public boolean getDireitaPressionada() {
+        return direitaPressionada;
+    }
+
+    public void setTiroPressionado(boolean tiroPressionado) {
+        this.tiroPressionado = tiroPressionado;
+    }
+
+    public boolean getTiroPressionado() {
+        return tiroPressionado;
+    }
+
+    public void setLogicaNecessariaNesteLoop(boolean logicaNecessariaNesteLoop) {
+        this.logicaNecessariaNesteLoop = logicaNecessariaNesteLoop;
+    }
+
+    public boolean getLogicaNecessariaNesteLoop() {
+        return logicaNecessariaNesteLoop;
+    }
+
+    public void setAllienCont(int allienCont) {
+        this.allienCont = allienCont;
+    }
+
+    public int getAllienCont() {
+        return allienCont;
+    }
+
+    public void setUltimoTiro(long ultimoTiro) {
+        this.ultimoTiro = ultimoTiro;
+    }
+
+    public long getUltimoTiro() {
+        return ultimoTiro;
+    }
+
+    public void setIntervaloDeTiro(long intervaloDeTiro) {
+        this.intervaloDeTiro = intervaloDeTiro;
+    }
+
+    public long getIntervaloDeTiro() {
+        return intervaloDeTiro;
+    }
+
+    public void setVelocidadeDeMovimento(double velocidadeDeMovimento) {
+        this.velocidadeDeMovimento = velocidadeDeMovimento;
+    }
+
+    public double getVelocidadeDeMovimento() {
+        return velocidadeDeMovimento;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setEntidades(ArrayList entidades) {
+        this.entidades = entidades;
+    }
+
+    public ArrayList getEntidades() {
+        return entidades;
+    }
+
+    public void setRemoveLista(ArrayList removeLista) {
+        this.removeLista = removeLista;
+    }
+
+    public ArrayList getRemoveLista() {
+        return removeLista;
     }
 }
