80940089fe65541872589999359978294e4e4820
douglas <douglas@douglas-Inspiron-3442>
2016-10-16 11:21:29 -0300

    Java doc adicionado.

--- src/spaceinvaders/Launcher.java
+++ src/spaceinvaders/Launcher.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import spaceinvaders.game.Game;
 
 /**
+ * Classe principal do aplicativo, responsável por criar a instância do classe
+ * Game e iniciar o loop principal do jogo.
  *
  * @author douglas
  * @version 1- 25/09/2016, 19:36:41
@@ -11,6 +13,13 @@ import spaceinvaders.game.Game;
  */
 public class Launcher {
 
+    /**
+     * Main da classe, responsável por instanciar a classe Game e inicializar o
+     * loop principal do jogo.
+     *
+     * @param args
+     * @throws IOException
+     */
     public static void main(String[] args) throws IOException {
         Game spaceInvaders = new Game();
         spaceInvaders.gameLoop();
